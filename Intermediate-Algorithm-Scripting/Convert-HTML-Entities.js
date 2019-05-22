721c7ae2a73f7ecf91690185707e45affcf51c31
Mario A <quaaludes@gmail.com>
2019-05-22 12:03:10 +0200

    Create Convert-HTML-Entities.js
    Added solution to Convert-HTML-Entities

---
+++ Intermediate-Algorithm-Scripting/Convert-HTML-Entities.js
@@ -0,0 +1,24 @@
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+convertHTML("Dolce & Gabbana") should return Dolce &​amp; Gabbana.
+Passed
+convertHTML("Hamburgers < Pizza < Tacos") should return Hamburgers &​lt; Pizza &​lt; Tacos.
+Passed
+convertHTML("Sixty > twelve") should return Sixty &​gt; twelve.
+Passed
+convertHTML('Stuff in "quotation marks"') should return Stuff in &​quot;quotation marks&​quot;.
+Passed
+convertHTML("Schindler's List") should return Schindler&​apos;s List.
+Passed
+convertHTML("<>") should return &​lt;&​gt;.
+Passed
+convertHTML("abc") should return abc.
+*/
+
+function convertHTML(str) {
+  // &colon;&rpar;
+  let char = {'&':'&amp;', '<': '&lt;', '>': '&gt;', '"':'&quot;', "'":'&apos;'}
+  return str.replace(/[&<>"']/g, c=>char[c]);
+}
+
+convertHTML("Dolce & Gabbana");
